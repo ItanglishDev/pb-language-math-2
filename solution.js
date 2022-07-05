4b72e2b60202b248963b830fb23014150c5e7afd
chris <chrishethertonweb@gmail.com>
2022-07-05 14:43:01 +0200

    exercise 3 solved

--- solution.js
+++ solution.js
@@ -2,3 +2,8 @@ let a = 3;
 let b = 4;
 let c = Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
 console.log(c);
+
+// exercise two
+
+let radCircle = 1;
+console.log("The area is ", 3.14 * Math.pow(radCircle, 2));
