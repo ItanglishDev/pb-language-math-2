d02d89e1f20c494ac66a6fc687bc09aca6fc9e35
chris <chrishethertonweb@gmail.com>
2022-07-05 15:00:24 +0200

    exercise 2 solved

--- solution.js
+++ solution.js
@@ -4,6 +4,9 @@ let c = Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
 console.log(c);
 
 // exercise two
+let maxNum = Math.max(3, 9, 1);
+let minNum = Math.min(1, 3, 9);
+console.log((maxNum + minNum) / 2);
 
 let radCircle = 1;
 console.log("The area is ", (Math.PI * Math.pow(radCircle, 2)).toFixed(3));
